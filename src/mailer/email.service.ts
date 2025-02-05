50e019dcf0a5a45f945aee12dd4e4f60c9fcfd66
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2025-02-05 16:10:04 +0200

    Update email.service.ts

--- src/mailer/email.service.ts
+++ src/mailer/email.service.ts
@@ -49,47 +49,4 @@ export class EmailService {
       }
     });
   }
-
-  //   async sendPasswordReset(email: string, token: string, isApp?: boolean) {
-  //     const webText = `Click on the link to set new password: <a href="${process.env.CLIENT_URL}/auth/new-password?token=${token}">Reset Password</a>`;
-  //     const appText = `Password recovery code: <p style="text-align: center; font-weight: bold; font-size: 30px;">${token}</p>`;
-
-  //     const mailOptions = {
-  //       from: process.env.GOOGLE_EMAIL,
-  //       to: email,
-  //       subject: 'Password Reset',
-  //       html: isApp ? appText : webText,
-  //     };
-
-  //     this.transporter.sendMail(mailOptions, (error, info) => {
-  //       if (error) {
-  //         this.logger.error(error);
-  //       } else {
-  //         this.logger.log(`Email sent to ${email}: ` + info.response);
-  //       }
-  //     });
-  //   }
-
-  //   async sendSetPasswordLetter(email: string) {
-  //     const text = `
-  //     <div style="font-family: Arial, sans-serif; line-height: 1.5;">
-  //       <h2 style="color: #4CAF50;">Your password has been successfully changed.</h2>
-  //      </div>
-  //   `;
-
-  //     const mailOptions = {
-  //       from: process.env.GOOGLE_EMAIL,
-  //       to: email,
-  //       subject: 'Password Reset',
-  //       html: text,
-  //     };
-
-  //     this.transporter.sendMail(mailOptions, (error, info) => {
-  //       if (error) {
-  //         this.logger.error(error);
-  //       } else {
-  //         this.logger.log(`Email sent to ${email}: ` + info.response);
-  //       }
-  //     });
-  //   }
 }
