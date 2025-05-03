8944ca46d99d99959a24acc953e19b60a7a548e8
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2025-05-03 15:59:23 +0300

    refactor: Nots

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,13 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { NotificationService } from './notification.service';
-import { SendNotificationDto } from './dto/send-notification.dto';
 
 @Controller('notification')
 export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
-
-  @Post()
-  async sendNotification(@Body() payload: SendNotificationDto) {
-    return await this.notificationService.sendNotification(payload);
-  }
 }
--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { SendNotificationDto } from './dto/send-notification.dto';
+
 import * as firebase from 'firebase-admin';
 import { BookType } from '@/common/enums/book.enum';
 import { AchievementName } from '@/common/enums/ach.enum';
@@ -9,52 +9,15 @@ import { achievementMessages } from './ach.messages';
 export class NotificationService {
   private readonly logger = new Logger(NotificationService.name);
 
-  async sendNotification(payload: SendNotificationDto) {
-    try {
-      const response = await firebase.messaging().send({
-        notification: {
-          title: payload.title,
-          body: payload.body,
-        },
-        token: payload.deviceId,
-        data: {},
-        android: {
-          priority: 'high',
-          notification: {
-            sound: 'default',
-            channelId: 'default',
-          },
-        },
-      });
-
-      this.logger.log('✅ The message has been successfully sent! Response ID:', response);
-      return {
-        success: true,
-        messageId: response,
-      };
-    } catch (error) {
-      this.logger.error('❌ Error sending a message:', error);
-
-      if (error.code === 'messaging/registration-token-not-registered') {
-        this.logger.warn(
-          '⚠️ The token is no longer valid. It should be removed from the database.',
-        );
-      }
-
-      return {
-        success: false,
-        error: error.message || 'Unknown error',
-      };
-    }
-  }
-
   async sendBigBookAch(deviceId: string) {
+    // 如果没有提供deviceId，则返回
     if (!deviceId) {
       this.logger.warn('⚠️ No deviceId provided for Big Book Achievement notification.');
       return;
     }
 
     try {
+      // 发送通知
       const response = await firebase.messaging().send({
         notification: {
           title: 'Congratulations! 🎉',
@@ -71,14 +34,17 @@ export class NotificationService {
         },
       });
 
+      // 记录发送成功的消息
       this.logger.log('✅ Big Book Achievement message sent! Response ID:', response);
       return {
         success: true,
         messageId: response,
       };
     } catch (error) {
+      // 记录发送失败的消息
       this.logger.error('❌ Error sending Big Book Achievement message:', error);
 
+      // 如果token无效，则记录警告信息
       if (error.code === 'messaging/registration-token-not-registered') {
         this.logger.warn(
           '⚠️ The token is no longer valid. It should be removed from the database.',
